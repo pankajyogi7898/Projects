e0ab1eaa68e69d1d9a9ebe0a663a912f4d7716ac
PANKAJ NATH <pyogi8461@gmail.com>
2026-07-18 11:18:53 +0530

    authentication complete

--- Yogi Ai/backend/server.js
+++ Yogi Ai/backend/server.js
@@ -2,12 +2,18 @@ import dotenv from 'dotenv';
 dotenv.config()
 import app from './src/app.js';
 import connectToDB from "./src/config/database.js"
+import { initSocket } from './src/sockets/server.socket.js';
+import http from "http"
 
+const httpServer = http.createServer(app);
 
+initSocket(httpServer);
 connectToDB()
+
+
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
--- Yogi Ai/frontend/src/features/Chat/pages/Dashboard.jsx
+++ Yogi Ai/frontend/src/features/Chat/pages/Dashboard.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useChat } from '../hooks/useAuth'
+import { useEffect } from 'react'
 
 const Dashboard = () => {
+    const chat = useChat()
 
     const { user } = useSelector(state => state.auth)
+
+    useEffect(() => {
+        chat.initializeSocketConnection
+    }, [])
+
     console.log(user)
+
     return (
         <>
             Dashboard
